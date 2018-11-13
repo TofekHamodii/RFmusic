be4d27f2d24ef45dc9f6f44f6096b1b943959549
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-11-13 18:23:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const PREFIX = "FR"
+const PREFIX = "RF"
 const Util = require('discord.js');
  
 const getYoutubeID = require('get-youtube-id');
